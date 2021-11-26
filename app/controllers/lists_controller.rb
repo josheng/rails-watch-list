0b0ef910e811cad2d1c70dec50bbbdfb946ca664
Joshua Eng <engch28@gmail.com>
2021-11-26 12:30:26 +0800

    updated to include photo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,6 +23,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :list_url)
+    params.require(:list).permit(:name, :list_url, :photo)
   end
 end
